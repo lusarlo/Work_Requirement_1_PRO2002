f75b0247de634a4701e583700cfc4b61176d2d91
Lucia Sarmiento Lodeiro <lusarlo@icloud.com>
2026-09-15 18:09:20 +0200

    refactor order creation and add readme inforamtion

--- README.md
+++ README.md
@@ -12,5 +12,13 @@ I decided to refactor this example following the Single Responsibility Principle
 By keeping responsibilities separate, the system is easier to understand, extend, and maintain.
 
 ## Order Creation Conditional Logic
+The original OrderProcessor used if/else statements to decide which order class should be created for each order type. This makes the creation logic harder to read and means that another conditional branch must be added when a new order type is needed.
+
+I refactored this example using a simple Factory pattern. The OrderFactory creates the correct order object. A dictionary maps each order type to its class, and the create_order method uses that mapping to create the object. It also checks whether the order type is supported and raises a ValueError when it is not.
+
+This improves the design by:
+- Removing the repeated if/else statements.
+- Making the code easier to read and maintain.
+- Making it easier to add another order type by updating only the factory.
 
 ## Vehicle Inheritance Issue
--- order_creation_conditional_logic.py
+++ order_creation_conditional_logic.py
@@ -1,19 +1,43 @@
-class OrderProcessor:
-    def create_order(self, order_type):
-        if order_type == "online":
-            return OnlineOrder()
-        elif order_type == "store":
-            return StoreOrder()
-        elif order_type == "phone":
-            return PhoneOrder()
-        else:
-            raise ValueError("Unknown order type")
+# class OrderProcessor:
+#     def create_order(self, order_type):
+#         if order_type == "online":
+#             return OnlineOrder()
+#         elif order_type == "store":
+#             return StoreOrder()
+#         elif order_type == "phone":
+#             return PhoneOrder()
+#         else:
+#             raise ValueError("Unknown order type")
+
+# class OnlineOrder:
+#     pass
+
+# class StoreOrder:
+#     pass
+
+# class PhoneOrder:
+#     pass
 
 class OnlineOrder:
-    pass
+    pass    
 
 class StoreOrder:
     pass
 
 class PhoneOrder:
     pass
+
+class OrderFactory:
+  order_types = {
+    "online": OnlineOrder,
+    "store":  StoreOrder,
+    "phone": PhoneOrder
+  }
+
+  @classmethod
+  def create_order(cls, order_type):
+    if order_type not in cls.order_types:
+      raise ValueError("Unknown order type")
+    
+    return cls.order_types[order_type]()
+
